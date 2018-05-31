1bb4c411ca31bbfa88b8bee21b069db8ee1606c0
Mhamidi <hamidi.michael@outlook.fr>
2018-05-31 11:11:39 +0200

    regions

--- HelloWorldMVC/Models/Dal.cs
+++ HelloWorldMVC/Models/Dal.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace HelloWorldMVC.Models
+namespace HelloWorldMVC.Models 
 {
     public class Dal : IDisposable
     {
@@ -12,36 +12,27 @@ namespace HelloWorldMVC.Models
         public Dal()
         {
             db = new BddContext();
-    }
+        }
 
         public void Dispose()
         {
             db.Dispose();
         }
-        #region Product
-        public List<Product> GetProducts()
-        {
-            return db.Products.ToList();
-            //== select * from product en sql
-        }
 
-        public void AddProduct(Product p)
-        {
-            db.Products.Add(p);
-            //ajoute le produits
-            //== insert into en sql
-            db.SaveChanges();
-            //sauvegarde
-        }
-        #endregion Product
+
 
         #region client
+
+        #region GetClients
         public List<Client> GetClients()
         {
             return db.Clients.ToList();
             //== select * from product en sql
         }
 
+        #endregion
+
+        #region AddClients
         public void AddClients(Client c)
         {
             db.Clients.Add(c);
@@ -50,13 +41,96 @@ namespace HelloWorldMVC.Models
             db.SaveChanges();
             //sauvegarde
         }
+
+        #endregion
+
+
+        #region  Client GetClient
+        public Client GetClient(int id)
+        {
+            return db.Clients.FirstOrDefault(x => (x.Id == id));
+        }
+
+        #endregion
+
+        #region DeleteClient
+        public void DeleteClient(int id)
+        {
+            Client exist = db.Clients.FirstOrDefault(x => (x.Id == id));
+            if (exist != default(Client))
+            {
+                db.Clients.Remove(exist);
+            }
+        }
+
+        #endregion
+
+        #region UpdateClient
+        public void UpdateClient(Client c)
+        {
+            Client exist = db.Clients.FirstOrDefault(x => (x.Id == c.Id));
+            //va rechercher dans la liste de produits,puis compare les identidifiants pour retrouver le meme
+            if (exist != default(Client))//preferer default à "null" pour les cas ou le retour est 0 ou autre
+            {
+                exist.LastName = c.LastName;
+                exist.FirstName = c.LastName;
+
+            }
+        }
+
+        #endregion
+
+        #region GetClients Predicate
+        public List<Client> GetClients(Predicate<Client> predicate)
+        {
+
+            List<Client> result = new List<Client>();
+            //cree une liste de resultats
+            foreach (Client c in db.Clients)
+            //pour chaque client dans la base de donnée Client
+            {
+                if (predicate(c))
+                //si le predicat correspond au Client demandé(p)
+                {
+                    result.Add(c);
+                    //la liste ajoute le Client a result
+                }
+            }
+            return result;
+            //retourne la liste result
+        }
+
+        #endregion
+
         #endregion client
+
+        #region product
+
+        #region GetProducts
+        public List<Product> GetProducts()
+        {
+            return db.Products.ToList();
+            //== select * from product en sql
+        }
+        #endregion
+
+        #region AddProduct
+        public void AddProduct(Product p)
+        {
+            db.Products.Add(p);
+            //ajoute le produits
+            //== insert into en sql
+            db.SaveChanges();
+            //sauvegarde
+        }
+        #endregion
+
         #region Update Product
         public void UpdateProduct(Product p)
         {
-            Product exist = db.Products.FirstOrDefault(x =>(x.Id == p.Id));
+            Product exist = db.Products.FirstOrDefault(x => (x.Id == p.Id));
             //va rechercher dans la liste de produits,puis compare les identidifiants pour retrouver le meme
-            if(exist != default(Product))//preferer default à "null" pour les cas ou le retour est 0 ou autre
+            if (exist != default(Product))//preferer default à "null" pour les cas ou le retour est 0 ou autre
             {
                 exist.ProductDescription = p.ProductDescription;
                 exist.ProductName = p.ProductName;
@@ -65,11 +139,15 @@ namespace HelloWorldMVC.Models
         }
         #endregion
 
+        #region Product GetProduct
         public Product GetProduct(int id)
         {
-          return db.Products.FirstOrDefault(x => (x.Id == id));
+            return db.Products.FirstOrDefault(x => (x.Id == id));
         }
 
+        #endregion
+
+        #region DeleteProduct
         public void DeleteProduct(int id)
         {
             Product exist = db.Products.FirstOrDefault(x => (x.Id == id));
@@ -80,17 +158,20 @@ namespace HelloWorldMVC.Models
 
 
         }
+
+        #endregion
+
         #region GetProducts Predicate
         public List<Product> GetProducts(Predicate<Product> predicate)
         {
 
             List<Product> result = new List<Product>();
             //cree une liste de resultats
-            foreach(Product p in db.Products)
-                //pour chaque produit dans la base de donnée product
+            foreach (Product p in db.Products)
+            //pour chaque produit dans la base de donnée product
             {
-                if(predicate(p))
-                    //si le predicat correspond au produit demandé(p)
+                if (predicate(p))
+                //si le predicat correspond au produit demandé(p)
                 {
                     result.Add(p);
                     //la liste ajoute le produit a result
@@ -100,5 +181,7 @@ namespace HelloWorldMVC.Models
             //retourne la liste result
         }
         #endregion
+
+        #endregion product
     }
 }
